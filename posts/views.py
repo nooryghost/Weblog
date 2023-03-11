6ab54ac5013164dcca92d0b775b48a02e2451045
nooryghost <109327061+nooryghost@users.noreply.github.com>
2023-03-11 22:07:30 +0330

    views added

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Post
+
+class PostListView(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by("-created_time")
+    template_name = "index.html"
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+class PostDeleteView(generic.DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("")
+
