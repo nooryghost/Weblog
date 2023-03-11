574dbe3605286373fddcf56b0dd804f73a83b251
nooryghost <109327061+nooryghost@users.noreply.github.com>
2023-03-11 21:39:54 +0330

    models and admin complated in posts app

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("author", "title", "status", "created_time")
+    list_filter = ("status",)
+    search_fields = ["title", "author", "description"]
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+STATUS = (
+    ("Published", 1),
+    ("Draft", 2)
+)
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.PositiveSmallIntegerField(choices=STATUS, default=2)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-created_time")
