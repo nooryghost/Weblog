b06dc18d7764808e4557c3a01fb386beeed5e2e5
nooryghost <109327061+nooryghost@users.noreply.github.com>
2023-03-11 21:47:33 +0330

    forms complated in posts app

---
+++ posts/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import Post
+from users.models import User
+
+class RegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2", "avatar", "description"]
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
