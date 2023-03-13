ac5030f5e16bd225efe03ee7e7ff2017835cb394
nooryghost <109327061+nooryghost@users.noreply.github.com>
2023-03-13 18:57:32 +0330

    posts app model upgraded

--- posts/models.py
+++ posts/models.py
@@ -3,23 +3,27 @@ from django.utils import timezone
 
 from users.models import User
 
-STATUS = (
-    ("Published", 1),
-    ("Draft", 2)
-)
-
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISH = "PB", "Published"
+        REJECTED = "RJ", "Rejected"
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_posts")
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
     description = models.TextField()
-    status = models.PositiveSmallIntegerField(choices=STATUS, default=2)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     publish = models.DateTimeField(default=timezone.now)
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ["-publish"]
+        indexes = [
+            models.Index(fields=["-publish"])
+        ]
 
     def __str__(self):
         return self.title
